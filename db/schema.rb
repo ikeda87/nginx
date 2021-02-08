8d51f4732c64c8637aff4f36ecd8f140b2fe0a52
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-08 09:17:47 +0000

    add migrate

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2021_02_08_084631) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
