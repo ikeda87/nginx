417d3d85f0087a62e16b32ea6b11df3ed7bc7ee4
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-08 09:02:39 +0000

    add route

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,10 +11,34 @@ class BlogsController < ApplicationController
   def edit
   end
   def create
+    @blog = Blog.new(blog_params)
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog, notice: "作成されました" }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
   end
   def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: "更新されました" }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
   end
   def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: "削除されました" }
+      format.json { head :no_content }
+    end
   end
   private
     def set_blog
