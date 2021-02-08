d4aa382aec4457f2e1fa4ab166a15a01977bf8c5
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-08 08:51:03 +0000

    add blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,18 @@
 class BlogsController < ApplicationController
-end
+  before_action :set_blog, only: %i[ show edit update destroy ]
+  def index
+  end
+  def show
+  end
+  def new
+  end
+  def edit
+  end
+  def create
+  end
+  def update
+  end
+  def destroy
+  end
+  private
+  end
